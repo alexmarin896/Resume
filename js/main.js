483a160955fc171de1a12c5921969975684cbacb
alexmarin896 <57626826+alexmarin896@users.noreply.github.com>
2024-04-14 07:50:43 +0300

    Commit 20
    Menu retracts after i press the link button in menu

--- js/main.js
+++ js/main.js
@@ -62,6 +62,29 @@
     //    slidesToScroll: 1
    // });
     
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").on('click', function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNav",
+        offset: 74,
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).on('scroll', navbarCollapse);
     
     // Back to top button
     $(window).scroll(function () {
